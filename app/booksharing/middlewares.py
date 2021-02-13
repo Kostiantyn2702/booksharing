2d4007052f92ae264c0ea6bc94d54bcf46484179
Kostiantyn27 <kostia.fedoroff1@gmail.com>
2021-02-13 15:23:23 +0200

    fix PEP8

--- app/booksharing/middlewares.py
+++ app/booksharing/middlewares.py
@@ -12,8 +12,8 @@ class SimpleMiddleware:
         end = time()
         print_log = Log.objects.create(path=request.path, method=request.method, time=end-start)
 
-        print(f"Path: {print_log.path}")
-        print(f"Method: {print_log.method}")
-        print(f"Requset-response took: {print_log.time}")
+        print(f"Path: {print_log.path}") # noqa
+        print(f"Method: {print_log.method}") # noqa
+        print(f"Requset-response took: {print_log.time}") # noqa
 
         return response
