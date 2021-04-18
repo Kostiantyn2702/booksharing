960ae961e5494fe509eec3bd0fd196c4963436a2
Kostiantyn27 <kostia.fedoroff1@gmail.com>
2021-04-18 17:56:13 +0300

    fix test_my_profile_update

--- app/tests/tests_my_profile_update.py
+++ app/tests/tests_my_profile_update.py
@@ -1,25 +1,50 @@
 from django.urls import reverse
+from accounts.models import User
+
+
+def login_user(client):
+    login_url = reverse('login')
+
+    # create user
+    password = 'helloWorld123'
+    email = 'test_login_success@mail.com'
+    avatar = 'avatars/47/IMG_20210320_000433.jpg'
+    user = User(email=email, username=email, avatar=avatar)
+    user.set_password(password)
+    user.save()
+
+    payload = {
+        'username': email,
+        'password': password,
+    }
+    client.post(login_url, data=payload)
+    return user
 
 
 def test_get_form(client):
     url = reverse('my-profile')
+    login_user(client)
     response = client.get(url, follow=True)
+
     assert response.status_code == 200
-    assert response.redirect_chain == [('/accounts/login/?next=/accounts/my-profile/', 302)]
 
 
 def test_empty_payload(client):
     url = reverse('my-profile')
+    login_user(client)
     payload = {}
-    response = client.patch(url, data=payload, follow=True)
+    response = client.put(url, data=payload, follow=True)
+
     assert response.status_code == 200
-    assert response.redirect_chain == [('/accounts/login/?next=/accounts/my-profile/', 302)]
+    assert response.redirect_chain == [('/', 302)]
 
 
 def test_valid_payload(client):
     url = reverse('my-profile')
-    payload = {"first_name": "first_name",
-               "last_name": "last_name"}
-    response = client.patch(url, data=payload, follow=True)
+    login_user(client)
+    payload = {"first_name": "test",
+               "last_name": "test",
+               "avatar": 'avatars/47/IMG_20210320_000433.jpg'}
+    response = client.put(url, data=payload, follow=True)
     assert response.status_code == 200
-    assert response.redirect_chain == [('/accounts/login/?next=/accounts/my-profile/', 302)]
+    assert response.redirect_chain == [('/', 302)]
